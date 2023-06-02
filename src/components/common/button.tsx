382e57adc00cd2b7dac9800f1876eca55f562c4e
Aidyn Ibrayev <aidyn.nis12@gmail.com>
2023-06-02 20:01:05 +0600

    feat(components/common): button new UI primitive

---
+++ src/components/common/button.tsx
@@ -0,0 +1,18 @@
+import { PropsWithChildren } from 'react';
+
+interface ButtonProps {
+	onClick: () => void;
+}
+
+const Button = ({ children, onClick }: PropsWithChildren<ButtonProps>) => {
+	return (
+		<button
+			onClick={onClick}
+			className="bg-cyan-dark_grayish hover:bg-cyan-light_grayish active:bg-cyan-strong rounded text-cyan-very_dark_grayish w-full uppercase font-bold text-xl py-2"
+		>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
